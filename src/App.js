9d3d12ab247145b255f306cd82ac87e7d5da062d
ndakalajulius <julius.ndakala@student.moringaschool>
2023-05-10 17:38:52 +0300

    added  and styled navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Career from "./pages/Career"
-import Contact from "./pages/Contact"
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Career from "./pages/Career";
+import Contact from "./pages/Contact";
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/career' element={<Career/>} />
-      <Route path='/contact' element={<Contact/>} />
-      <Route path='/welcome' element={<Home/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/career" element={<Career />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
-      </Router>   
-     
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import './Navbar.css';
-import {  Link } from "react-router-dom";
-const Navbar= () =>{
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+const Navbar = () => {
   return (
-  <div className="navigation">
-    <div className="navigation-menu">
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/contact">Contact</Link>
-    </li>
-    <li>
-      <Link to="/about">About</Link>
-    </li>
-    <li>
-      <Link to="/career">Career</Link>
-    </li>
+    <div className="navigation">
+     <h1> SHOPIFY</h1>
+      <div className="navigation-menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/career">Career</Link>
+        </li>
+      </div>
     </div>
-  </div>
   );
-}
+};
 export default Navbar;
