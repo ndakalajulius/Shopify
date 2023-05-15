a0033b1010a9edfc59d8f33be752e8fc2cc65fa4
ndakalajulius <julius.ndakala@student.moringaschool>
2023-05-15 17:54:05 +0300

    editeed the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import "./Home.css";
 import Navbar from "../components/Navbar"
-
+import home from '../components/images/download5.jpeg';
 
 
 const Home = () => {
   return (<>
     <Navbar/>
-    
+    <div className='mainhomepage'>
+    <div className='homepage'>
+    <img src={home} alt="React home" width="393" height="330" />
+    </div>
+    </div>
   
     
     
