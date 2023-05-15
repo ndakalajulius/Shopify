3523e03bc86f297074ab7c7ae08d3400d5727d18
ndakalajulius <julius.ndakala@student.moringaschool>
2023-05-15 16:51:47 +0300

    made changes in the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from "react";
 import "./about.css";
-import Navbar from "../components/Navbar"
-const About = () => {
-  return (<>
-   
-    <div className='main-about'> 
-    <Navbar/>
-    <h1>  ABOUT US  </h1>
-    <h2> ----- Our Story -----</h2>
-    </div>
-    </>
-  )
-}
 
-export default About
+const About = () => {
+  return (
+    <>
+      <div className="main-about">
+        <h1> ABOUT US </h1>
+        <h2> ----- Our Story -----</h2>
+        <div className="text">
+        <p> In a fast-evolving eCommerce landscape, apart from great products and frequent innovation, what users want to see is the kind of collaborations and partnerships a brand establishes.
+
+        These reveal the general direction the brand is trying to evolve in and also put the spotlight on special efforts to make its products more differentiated and desirable. Put more simply, partnerships and collaborations reveal that a brand exists for reasons beyond the material and transactional. 
+        
+        This is how Made, the furniture brand, ensures people strolling through their about page get to know what they’ve been up to. </p>
+        
+        
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
