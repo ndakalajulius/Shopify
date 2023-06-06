381ff23e55bd6c4352769e643aa03cf7090c0cf8
julius ndakala <juliusndakala35@gmail.com>
2023-06-06 19:03:42 +0300

    styled landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,23 +1,56 @@
-import React from 'react'
-import './Landing.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Landing.css";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 const Landing = () => {
   return (
-    < >
-    <div className='main-landing'>
-    <h1 className="main-title text-center">login / register page</h1>
-    <p className="main-para text-center">join us now and don't waste time</p>
-    <div className="buttons text-center">
-        <Link to="/login">
-            <button className="primary-button">log in</button>
-        </Link>
-        <Link to="/register">
-            <button className="primary-button" id="reg_btn"><span>register </span></button>
-        </Link>
-    </div>
-    </div>
-    </>
-  )
-}
+    <>
+      <Navbar />
+      <section class="hero">
+        <div class="intro-text">
+          <h1>
+            <span class="hear"> You can Hear the Jabs </span> <br />
+            <span class="connecting"> Connecting</span>
+          </h1>
+          <p>
+            An online streaming platform for boxing matches <br />
+            We also dedicate some special time to throwbacks cuz old is gold
+          </p>
+          <a class="btn red" href="#">Learn More</a>
+          <a class="btn blue" href="#">Subscribe</a>
+        </div>
+        <div class="i-frame">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/sUmM_PFpsvQ"
+            title="YouTube video player"
+            frameborder="10"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+          <div class="stand-1"></div>
+          <div class="stand-2"></div>
+        </div>
+      </section>
 
-export default Landing
+      <div className="main-landing">
+        <h1 className="main-title text-center">login / register page</h1>
+        <p className="main-para text-center">
+          join us now and don't waste time
+        </p>
+        <div className="buttons text-center">
+          <Link to="Signin/">
+            <button className="primary-button">log in</button>
+          </Link>
+          <Link to="/Signup">
+            <button className="primary-button" id="reg_btn">
+              <span>register </span>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Landing;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,11 @@
 import React from "react";
-import "./Home.css";
-import Navbar from "../components/Navbar";
-
-import SignInPage from "../components/SignInPage";
-import Footer from "../components/Footer";
+import Landing from "../components/Landing";
 
 const Home = () => {
   return (
-    <>
-   < div className="mainhomepage">
-      <Navbar />
-      <SignInPage />
-      <Footer/>
-      </div>
-    </>
+    <div>
+      <Landing />
+    </div>
   );
 };
 
