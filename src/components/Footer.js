c0903ed63c2026238a091c207e4704eb30b48979
julius ndakala <juliusndakala35@gmail.com>
2023-06-06 11:42:42 +0300

    made changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,44 @@
-import React from 'react'
-
-
+import React from "react";
+import "./Footer.css";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Footer = () => {
-  return (<>
-	<div id='social'>
+  return (
+    <div className="footer">
+      <div classname="social-container">
+        <h3>Social Follow</h3>
+        <a
+          href="https://www.youtube.com/c/jamesqquick"
+          className="youtube social"
+        >
+          <FontAwesomeIcon icon={faYoutube} size="2x" />
+        </a>
+        <a
+          href="https://www.facebook.com/learnbuildteach/"
+          className="facebook social"
+        >
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a
+          href="https://www.twitter.com/jamesqquick"
+          className="twitter social"
+        >
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+        <a
+          href="https://www.instagram.com/learnbuildteach"
+          className="instagram social"
+        >
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+      </div>
+    </div>
+  );
+};
 
-  <a className='fa fa-linked-in' target='_blank' rel='noopener noreferrer' href='https://linkedin.com/in/sample-7842b814a'></a>
-  <a className='fa fa-github' target='_blank' rel='noopener noreferrer' href='https://github.com/sample'></a>
-  <a className='fa fa-google' rel='noopener noreferrer' href="mailto:sample@gmail.com"></a>
-  <a className='fa fa-instagram' target='_blank' rel='noopener noreferrer' href='https://www.instagram.com/sample/?hl=undefined'></a>
-
-</div>
-
-<div id='elements'>
-
-  <img id='phone' src={phone} />
-  <p>+32 696 69 69 69</p>
-
-  <img id='email' src={email} /> <p>sample@gmail.com</p>
-  <img id='pin' src={pin} /> <p>Antwerp , Belgium</p>
-</div>
-</>
-  )
-}
-
-export default Footer
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,14 @@ import "./Home.css";
 import Navbar from "../components/Navbar";
 
 import SignInPage from "../components/SignInPage";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <SignInPage />
-      
+      <Footer/>
     </>
   );
 };
