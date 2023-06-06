55704bbe93e7dbaa4a115c19f8bd9dbb525c33ee
julius ndakala <juliusndakala35@gmail.com>
2023-06-06 20:18:50 +0300

    Added changes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Landing.css";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 const Landing = () => {
   return (
     <>
@@ -16,39 +17,37 @@ const Landing = () => {
             An online streaming platform for boxing matches <br />
             We also dedicate some special time to throwbacks cuz old is gold
           </p>
-          <a class="btn red" href="#">Learn More</a>
-          <a class="btn blue" href="#">Subscribe</a>
+          <a class="btn red" href="./about">
+            Learn More
+          </a>
+          <a class="btn blue" href="./contact">
+            Contact
+          </a>
         </div>
         <div class="i-frame">
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/sUmM_PFpsvQ"
-            title="YouTube video player"
-            frameborder="10"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          ></iframe>
+          <div className="main-landing">
+            <p className="main-para text-center">
+              Join us now and don't waste time
+            </p>
+            <div className="buttons text-center">
+              <Link to="Signin/">
+                <button className="primary-button">
+                  {" "}
+                  <span> Log in</span>
+                </button>
+              </Link>
+              <Link to="/Signup">
+                <button className="primary-reg-button" id="reg_btn">
+                  <span>Register </span>
+                </button>
+              </Link>
+            </div>
+          </div>
           <div class="stand-1"></div>
           <div class="stand-2"></div>
         </div>
       </section>
-
-      <div className="main-landing">
-        <h1 className="main-title text-center">login / register page</h1>
-        <p className="main-para text-center">
-          join us now and don't waste time
-        </p>
-        <div className="buttons text-center">
-          <Link to="Signin/">
-            <button className="primary-button">log in</button>
-          </Link>
-          <Link to="/Signup">
-            <button className="primary-button" id="reg_btn">
-              <span>register </span>
-            </button>
-          </Link>
-        </div>
-      </div>
+      <Footer />
     </>
   );
 };
